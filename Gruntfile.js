d74868d0defb45f53cd748ca7e13b2d9d15ebfff
Shaw W Chu <shawwchu@gmail.com>
2016-05-13 14:59:14 +1200

    Npm installed grunt-browersify along with babelify, added runtime support via babel runtime.
    Configured Gruntfile.js with browserify task, and a develop task within it.
    Task's options set to use babelify transform. Also enabled debug settings.
    
    Npm installed runtime transform plugin as dev-dependency, and the actual
    runtime as a dependency. Configured .babelrc to use this runtime transform.

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,14 +11,34 @@ module.exports = function (grunt) {
       }
     },
 
-    watch : {
-      // context wide options
-      options : {
-        livereload : true
-      }, 
+    browserify : {
+      develop : {
+        options : {
+          transform : [ ['babelify'] ],
 
+          browserifyOptions : {
+            debug: true 
+          }
+        },
+
+        files : {
+          'public/js/bundle.js' : 'src/app.js' 
+        }
+      }
+    },
+
+    watch : {
       static : {
+        options : {
+          livereload : true 
+        },
+
         files : ['public/css/**', 'public/js/**', 'public/index.html']
+      },
+
+      develop : {
+        files : ['src/**/*.js'],
+        tasks : ['browserify']
       }
     }
 
@@ -26,6 +46,7 @@ module.exports = function (grunt) {
 
   // load the grunt task package
   grunt.loadNpmTasks('grunt-contrib-connect')
+  grunt.loadNpmTasks('grunt-browserify')
   grunt.loadNpmTasks('grunt-contrib-watch')
 
   // register task, need a default at least
